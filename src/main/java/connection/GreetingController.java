b612af0725284694476dc008b9f74663f0186710
Vasua Pupkin <martyr552@mail.ru>
2019-03-13 21:06:05 +0300

    Java + Spring + Maven + PostgreSQL, news_feed web, add user authorization

--- src/main/java/connection/GreetingController.java
+++ src/main/java/connection/GreetingController.java
@@ -13,44 +13,48 @@ import java.util.List;
 import java.util.Map;
 
 @Controller // связующий элемент между моделью и видом
-public class GreetingController {
+public class GreetingController {// контролер модуль который по пути greeting слушает запросы пользователя и возвр какие то данные
     @Autowired //автоматическая привязка сервиса к укзанной переменной типа репо и установка связи с бд
     private MessageRepo messageRepo;
-    @GetMapping("/greeting") // контролер модуль который по пути greeting слушает запросы пользователя и возвр какие то данные
-    // в тукущем примере он будет возвращать файл шаблона приветсвия (greeting.mustache) метод GET выдергивает данные от польз.из url
+   /* @GetMapping("/")
+    // анотация - ярлык RequestParam, которые выдергивает данные по укзаны полям от польз.из url
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
-         // метоод ожидает на вход параметр имя ( этот параметр не обязательный  required = false, и дефолтное значение у него мирр)
+         // метоод ожидает на вход параметр имя ( required = false, и дефолтное значение у него World - эти поля не обяз.)
         model.put("name", name); // тут мы храним данные для дальнейшего возврата пользователю
-        return "greeting"; // возвращаем имя файла спинговому контейнеру которое мы хотим отобразить
+        return "greeting"; // возвращаем имя файла-шаблона спинговому контейнеру которое мы хотим отобразить
+    }*/
+    @GetMapping("/")
+     public String mainPage( Map<String,Object> model){
+       return "greeting" ;
     }
-    @GetMapping  // мепирование // если не указывать функцию в параметре то отображаться будет шаблон main
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
        // Iterable<Message> messages = messageRepo.findAll(); // подключаем репозирии с данными
-
-        Object messages1 = model.put("messages",  messageRepo.findAll()); //в map складываю то что вернулсь мне от пользователя в main.mustache
+          Object messages1 = model.put("messages",  messageRepo.findAll()); //в map складываю то что вернулсь мне от пользователя в main.mustache
         //(это отображается как лист в {{#messages}})
 
         return "main";
     }
 
-    @PostMapping // методы запрса выдергивает данные из формы (см main.mustache <form method="post">) postMapping к form method ="post",
+    @PostMapping("/main") // методы запрса выдергивает данные из формы (см main.mustache <form method="post">) postMapping к form method ="post",
      // антотация @RequestParam берет значения из блока post по аналогичным именам переменнах в
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(@RequestParam String text, @RequestParam String tag) {
+        Map<String, Object> model =null;
+        Message message = new Message(text, tag); //  складываем данные от пользователя в переменные класса сущности Message
 
-        messageRepo.save(message);
+        messageRepo.save(message);//сохраняем в БД информацию , которую выдернули RequestParam
 
-        Iterable<Message> messages = messageRepo.findAll(); // метод findAll = возвращает Iterable
+        Iterable<Message> messages = messageRepo.findAll(); //  берем информацию из репозиторя (БД)
 
-        model.put("messages",messages);
+        model.put("messages",messages);// кладем в модель
 
-        return "main";
+        return "main"; // отдали пользователю
     }
     @PostMapping("filter")
      public String filter (@RequestParam String filter, Map <String,Object> model){ // то что мы принемаем из блока post filter
         Iterable<Message> messages;
            if(filter!=null&!filter.isEmpty()){
-        messages = messageRepo.findByTag(filter);}
+        messages = messageRepo.findByTag(filter);} // CrudRepository передаем текст из поля filter
            else {
             messages = messageRepo.findAll();}
              model.put("messages", messages);
--- src/main/java/connection/repos/MessageRepo.java
+++ src/main/java/connection/repos/MessageRepo.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
-    List<Message> findByTag (String tag);
+    List<Message> findByTag (String tag); // поиск по тегу, остальные правила
+    // см в рук-во по spring jpa https://docs.spring.io/spring-data/jpa/docs/1.5.0.RELEASE/reference/html/jpa.repositories.html#jpa.query-methods.query-creation
 }
